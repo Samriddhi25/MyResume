0718dc80127a9457681cdb97608b2b501c191204
Samriddhi Agarwal <samriddhiagarwal12@gmail.com>
2020-06-11 02:57:27 +0530

    myresume

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://Samriddhi25.github.io/",
+    "name": "Samriddhi Agarwal",
+    "role": "Frontend Developer and UI/UX Designer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Currently a freshman and working on many academic and club projects.Actively participating in Open source projects  and NCC activities",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://in.linkedin.com/in/samriddhiagarwal25",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Samriddhi25",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a freshman student at The Pranveer Singh Institute of Technology and pursuing my B.Tech from here. I am a self taught Front-end Web Developer, currently diving into Machine Learning and Designing. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://Samriddhi25.github.io",
+    "education":[
+      {
+        "UniversityName":"Pranveer Singh Institute of Technology",
+        "specialization":"Information Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        //"Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Delhi Public School",
+        "specialization":"Science",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "Achievements":"Head Girl"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Skynox Tech.",
+        "specialization":"Front-end Intern",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2020",
+        //"Achievements":"Some Achievements"
+      },
+      /*{
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }*/
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
