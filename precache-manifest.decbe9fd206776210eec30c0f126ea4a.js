70a5de398a5ccb4ba950514f032765971cea978f
Samriddhi Agarwal <samriddhiagarwal12@gmail.com>
2020-06-11 21:39:09 +0530

    Updates

---
+++ precache-manifest.decbe9fd206776210eec30c0f126ea4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c286ff8a186664a547d",
+    "url": "/MyResume/static/css/main.7b8aef23.chunk.css"
+  },
+  {
+    "revision": "3c286ff8a186664a547d",
+    "url": "/MyResume/static/js/main.3c286ff8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyResume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "8ac872a9ef13b0bbbf13",
+    "url": "/MyResume/static/js/runtime~main.8ac872a9.js"
+  },
+  {
+    "revision": "c3d1b354e0f699e75630398f876bbae9",
+    "url": "/MyResume/index.html"
+  }
+];
